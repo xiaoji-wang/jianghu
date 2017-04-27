41d7f6c2674b63fa3b02f31d87372f6190b4cf1c
wxjivm@163.com <vmoa5304>
2017-04-27 23:36:38 +0800

    1

--- src/components/World.vue
+++ src/components/World.vue
@@ -11,18 +11,21 @@
       </select>
     </div>
     <div style="height: 50%;overflow: hidden;position: relative;background: #888;">
-      <div style="position: absolute;background: #444;" :style="center">
-        <div style="position: absolute;" v-for="i in (row * col)" :style="{left: left(i),top: top(i)}"
-             @click="move(i - 1 - Math.floor((i - 1) / col) * col, Math.floor((i - 1) / col))">
-          <div class="hexagon hexagon1" :style="hexagon"></div>
-          <div class="hexagon hexagon2" :style="hexagon"></div>
-          <div class="hexagon" :style="hexagon">
+      <div style="position: absolute;background: #444;transition: margin-top,margin-left 1s,1s;" :style="center">
+        <div style="position: absolute;" v-for="i in (row * col)" :style="{left: left(i) + 'px', top: top(i) + 'px'}">
+          <div class="hexagon hexagon1" :style="hexagon"
+               :class="{current: current(i - 1 - Math.floor((i - 1) / col) * col, Math.floor((i - 1) / col))}"></div>
+          <div class="hexagon hexagon2" :style="hexagon"
+               :class="{current: current(i - 1 - Math.floor((i - 1) / col) * col, Math.floor((i - 1) / col))}"></div>
+          <div class="hexagon" :style="hexagon"
+               :class="{current: current(i - 1 - Math.floor((i - 1) / col) * col, Math.floor((i - 1) / col))}"
+               @click="move(i - 1 - Math.floor((i - 1) / col) * col, Math.floor((i - 1) / col))">
             <span>{{i - 1 - Math.floor((i - 1) / col) * col}}, {{Math.floor((i - 1) / col)}}</span>
           </div>
         </div>
       </div>
       <div
-        style="position: absolute;left: 50%;top: 50%;width: 34px;height: 40px;margin-top: -21px;margin-left: -18px;z-index: 2;border: solid 1px white;"></div>
+        style="position: absolute;left: 50%;top: 50%;width: 34px;height: 40px;margin-top: -20px;margin-left: -17px;z-index: 2;border: solid 1px white;"></div>
     </div>
   </div>
 </template>
@@ -32,23 +35,20 @@
     name: 'world',
     methods: {
       top (i) {
-        var y = Math.floor((i - 1) / this.col)
-        return y * 1.5 * this.lengthen - y + 'px'
+        let y = Math.floor((i - 1) / this.col)
+        return y * 1.5 * this.lengthen - y
       },
       left (i) {
-        var y = Math.floor((i - 1) / this.col)
-        var x = i - y * this.col - 1
-        return (y % 2 === 0 ? x * this.width : x * this.width + this.width / 2) + (x - 1) + 'px'
+        let y = Math.floor((i - 1) / this.col)
+        let x = i - y * this.col - 1
+        return (y % 2 === 0 ? x * this.width : x * this.width + this.width / 2) + x
       },
       move (x, y) {
-        this.x.old = this.x.current
-        this.y.old = this.y.current
-        this.x.current = x
-        this.y.current = y
-        this.x.minus = this.x.current - this.x.old
-        this.y.minus = this.y.current - this.y.old
-        console.info(this.x)
-        console.info(this.y)
+        this.x = x
+        this.y = y
+      },
+      current (x, y) {
+        return x === this.x && y === this.y
       }
     },
     computed: {
@@ -57,14 +57,10 @@
       },
       center () {
         return {
-          top: (this.row + 2 - this.y.minus * 1.5) * this.lengthen + 'px',
-//            this.y % 2 === 0 ? (this.row - this.y) * this.lengthen : (this.row - this.y) * this.lengthen - this.lengthen / 2 + 'px',
-          left: (this.col - 2 - this.x.minus * 2) * this.width + 'px'
-//            this.x % 2 === 0 ? (this.col - this.x) * this.width - 2 * this.width : (this.col - this.x) * this.width - 2.5 * this.width + 'px'
-//          width: (this.width + 1) * this.col + this.width / 2 + 'px',
-//          height: (this.width - 2) * this.row + 'px'
-//          marginLeft: this.y * this.width * -1 + 'px',
-//          marginTop: this.x * this.lengthen * -1 + 'px'
+          top: '50%',
+          left: '50%',
+          marginTop: -1 * this.top(this.y * this.col + this.x + 1) - this.lengthen + 'px',
+          marginLeft: -1 * (this.left(this.x + 1) + (this.y % 2 === 0 ? this.width / 2 : this.width)) + 'px'
         }
       },
       hexagon () {
@@ -77,19 +73,11 @@
     },
     data () {
       return {
-        lengthen: 20,
-        row: 4,
-        col: 6,
-        x: {
-          old: 0,
-          current: 0,
-          minus: 0
-        },
-        y: {
-          old: 0,
-          current: 0,
-          minus: 0
-        }
+        lengthen: 40,
+        row: 10,
+        col: 10,
+        x: 5,
+        y: 5
       }
     }
   }
@@ -108,5 +96,9 @@
 
   .hexagon2 {
     transform: rotate(120deg);
+  }
+
+  .current {
+    background: wheat;
   }
 </style>
