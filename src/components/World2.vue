12f9084546aa83fa43a930448708cdb366f0a1c7
wxji <vmoa5304>
2017-05-03 19:40:56 +0800

    1

--- src/components/World2.vue
+++ src/components/World2.vue
@@ -1,70 +1,108 @@
 <template>
-  <div style="height: 100%;">
-    <div style="height: 20%;">
+  <div style="height: 100%;background: black;">
+    <div style="height: 30%;">
       <router-link to="/login">123</router-link>
     </div>
-    <div style="height: 50%;background: #666;">
+    <div style="height: 40%;">
       <canvas id="canvas" @click="move($event)"></canvas>
     </div>
   </div>
 </template>
-
 <script>
   export default {
     name: 'world2',
     computed: {
+      lengthen () {
+        return Math.floor(this.canvas.height / 5)
+      },
       width () {
-        return Math.floor(this.lengthen * 0.866)
+        return (Math.floor(this.lengthen * 0.866) << 1)
+      },
+      halfWidth () {
+        return this.width >> 1
+      },
+      canvas () {
+        return document.getElementById('canvas')
+      },
+      centerPixels () {
+        return this.axisToPixels({x: 0, y: 0})
       }
     },
     methods: {
       move (e) {
-        let canvas = this.getCanvas()
-        this.current.x = Math.round((e.offsetX - (canvas.width >> 1)) / (this.width << 1))
-        this.current.y = Math.round((e.offsetY - (canvas.height >> 1)) / (this.lengthen << 1))
-        console.info(this.current)
+        this.pixelsPoint.click.x = e.offsetX
+        this.pixelsPoint.click.y = e.offsetY
       },
-      drawHexagon (canvas, ctx, x, y, text) {
-        let lx = (canvas.width >> 1) + x * (this.width << 1) + (y % 2 === 0 ? 0 : this.width)
-        let ly = (canvas.height >> 1) + y * ((this.lengthen << 1) - (this.lengthen >> 1))
-        let halfHeight = this.lengthen
+      axisToPixels (axis) {
+        return {
+          x: (this.canvas.width >> 1) + axis.x * this.width + (axis.y % 2 === 0 ? 0 : this.halfWidth),
+          y: (this.canvas.height >> 1) + axis.y * ((this.lengthen << 1) - (this.lengthen >> 1))
+        }
+      },
+      drawHexagon (ctx, x, y, text) {
+        let pixels = this.axisToPixels({x: x, y: y})
+        let lx = pixels.x
+        let ly = pixels.y
+//        let offsetX = 0
+        if (this.pixelsPoint.offset.x !== Math.floor(this.w)) {
+          if (this.pixelsPoint.offset.x < 0) {
+            this.w -= 0.5
+          } else if (this.pixelsPoint.offset.x > 0) {
+            this.w += 0.5
+          }
+//          lx += this.w
+//          this.pixelsPoint.offset.x--
+//          offsetX = 100 - this.pixelsPoint.offset.x
+        } else {
+//          this.pixelsPoint.offset.x = 0
+//          this.w = 0
+        }
+//        console.info(this.w)
         let quarterHeight = this.lengthen >> 1
-        let halfWidth = this.width
         ctx.beginPath()
-        ctx.moveTo(lx - halfWidth, ly - quarterHeight)
-        ctx.lineTo(lx, ly - halfHeight)
-        ctx.lineTo(lx + halfWidth, ly - quarterHeight)
-        ctx.lineTo(lx + halfWidth, ly + quarterHeight)
-        ctx.lineTo(lx, ly + halfHeight)
-        ctx.lineTo(lx - halfWidth, ly + quarterHeight)
-        ctx.lineTo(lx - halfWidth, ly - quarterHeight)
-        if (this.current.x === x && this.current.y === y) {
+        ctx.moveTo(lx - this.halfWidth + this.w, ly - quarterHeight)
+        ctx.lineTo(lx + this.w, ly - this.lengthen)
+        ctx.lineTo(lx + this.halfWidth + this.w, ly - quarterHeight)
+        ctx.lineTo(lx + this.halfWidth + this.w, ly + quarterHeight)
+        ctx.lineTo(lx + this.w, ly + this.lengthen)
+        ctx.lineTo(lx - this.halfWidth + this.w, ly + quarterHeight)
+        ctx.lineTo(lx - this.halfWidth + this.w, ly - quarterHeight)
+        if (ctx.isPointInPath(this.pixelsPoint.click.x, this.pixelsPoint.click.y)) {
+          if (this.axisPoint.old.x !== x || this.axisPoint.old.y !== y) {
+            this.axisPoint.old.x = x
+            this.axisPoint.old.y = y
+            this.pixelsPoint.offset.x = this.centerPixels.x - lx
+            this.pixelsPoint.offset.y = this.centerPixels.y - ly
+            this.centerPixels = {x: lx, y: ly}
+//            console.info(this.pixelsPoint.offset.x + ',' + this.pixelsPoint.offset.y)
+          }
+          this.axisPoint.current.x = x
+          this.axisPoint.current.y = y
           ctx.fillStyle = 'wheat'
           ctx.fill()
+          ctx.fillStyle = 'black'
+          ctx.fillText(text, lx + this.w, ly)
         } else {
           ctx.stroke()
+          ctx.fillStyle = 'white'
+          ctx.fillText(text, lx + this.w, ly)
         }
         ctx.closePath()
-        ctx.textAlign = 'center'
-        ctx.textBaseline = 'middle'
-        ctx.fillStyle = 'black'
-        ctx.fillText(text, lx, ly)
-      },
-      getCanvas () {
-        return document.getElementById('canvas')
       }
     },
     mounted () {
-      let canvas = this.getCanvas()
-      canvas.width = window.innerWidth
-      canvas.height = canvas.parentNode.clientHeight
-      let ctx = canvas.getContext('2d')
+      this.canvas.width = window.innerWidth
+      this.canvas.height = this.canvas.parentNode.clientHeight
+      let ctx = this.canvas.getContext('2d')
+      this.pixelsPoint.click = {x: this.canvas.width >> 1, y: this.canvas.height >> 1}
       ctx.strokeStyle = 'wheat'
+      ctx.textAlign = 'center'
+      ctx.textBaseline = 'middle'
       let fn = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         for (let y = -3; y < 4; y++) {
           for (let x = -3; x < 4; x++) {
-            this.drawHexagon(canvas, ctx, x, y, x + ',' + y)
+            this.drawHexagon(ctx, x, y, x + ',' + y)
           }
         }
         window.requestAnimationFrame(fn)
@@ -73,13 +111,17 @@
     },
     data () {
       return {
-        lengthen: 50,
-        current: {x: 0, y: 0}
+        axisPoint: {
+          old: {x: 0, y: 0},
+          current: {x: 0, y: 0}
+        },
+        pixelsPoint: {
+          offset: {x: 0, y: 0},
+          click: {x: 0, y: 0}
+        },
+        w: 0
       }
     }
   }
 </script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
