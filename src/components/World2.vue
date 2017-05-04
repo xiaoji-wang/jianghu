42d6ffd9fbe4f347a65c913ff88ab23a3d366269
wxji <vmoa5304>
2017-05-04 19:47:38 +0800

    1

--- src/components/World2.vue
+++ src/components/World2.vue
@@ -45,33 +45,39 @@
         let ly = pixels.y
         if (this.pixelsPoint.offset.x !== Math.trunc(this.pixelsPoint.offset.tx)) {
           if (this.pixelsPoint.offset.x > 0) {
-            this.pixelsPoint.offset.tx += 0.1
+            this.pixelsPoint.offset.tx += 0.2
           } else if (this.pixelsPoint.offset.x < 0) {
-            this.pixelsPoint.offset.tx -= 0.1
+            this.pixelsPoint.offset.tx -= 0.2
           }
           lx += this.pixelsPoint.offset.tx
         } else {
-          lx += this.pixelsPoint.offset.x
+          this.pixelsPoint.offset.cx += this.pixelsPoint.offset.x
+//          this.pixelsPoint.offset.x = 0
+//          this.pixelsPoint.offset.tx = 0
+          lx += this.pixelsPoint.offset.cx
         }
         if (this.pixelsPoint.offset.y !== Math.trunc(this.pixelsPoint.offset.ty)) {
           if (this.pixelsPoint.offset.y > 0) {
-            this.pixelsPoint.offset.ty += 0.1
+            this.pixelsPoint.offset.ty += 0.28
           } else if (this.pixelsPoint.offset.y < 0) {
-            this.pixelsPoint.offset.ty -= 0.1
+            this.pixelsPoint.offset.ty -= 0.28
           }
           ly += this.pixelsPoint.offset.ty
         } else {
-          ly += this.pixelsPoint.offset.y
+          this.pixelsPoint.offset.cy += this.pixelsPoint.offset.y
+//          this.pixelsPoint.offset.y = 0
+//          this.pixelsPoint.offset.ty = 0
+          ly += this.pixelsPoint.offset.cy
         }
         ctx.beginPath()
         this.drawLine(ctx, lx, ly)
-        if (ctx.isPointInPath(this.pixelsPoint.click.x + this.pixelsPoint.offset.tx, this.pixelsPoint.click.y)) {
+        if (ctx.isPointInPath(this.pixelsPoint.click.x + this.pixelsPoint.offset.tx + this.pixelsPoint.offset.cx, this.pixelsPoint.click.y + this.pixelsPoint.offset.ty + this.pixelsPoint.offset.cy)) {
           if (this.axisPoint.old.x !== x || this.axisPoint.old.y !== y) {
             let oldPixelsPoint = this.axisToPixels(this.axisPoint.old)
             this.pixelsPoint.offset.x += (oldPixelsPoint.x - lx)
             this.pixelsPoint.offset.y += (oldPixelsPoint.y - ly)
-            this.axisPoint.old.x = x
-            this.axisPoint.old.y = y
+            this.axisPoint.old.x += x
+            this.axisPoint.old.y += y
             console.info(this.pixelsPoint.offset.x + ',' + this.pixelsPoint.offset.y)
           }
           this.axisPoint.current.x = x
@@ -126,10 +132,9 @@
           current: {x: 0, y: 0}
         },
         pixelsPoint: {
-          offset: {x: 0, y: 0, tx: 0, ty: 0},
+          offset: {x: 0, y: 0, tx: 0, ty: 0, cx: 0, cy: 0},
           click: {x: 0, y: 0}
-        },
-        w: 0
+        }
       }
     }
   }
