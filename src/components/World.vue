3ee20d8b056a7b25ee185f312fbfd81ff65ad2a4
wxji <wxjivm@163.com>
2017-07-26 22:40:03 +0800

    1

--- src/components/World.vue
+++ src/components/World.vue
@@ -31,7 +31,7 @@
         return {x: this.canvas.width >> 1, y: this.canvas.height >> 1}
       },
       distanceX () {
-        return 0.1
+        return 0.2
       },
       distanceY () {
         return this.distanceX * 1.73
@@ -49,14 +49,19 @@
         return this.pixelsPoint.offset.x !== 0 || this.pixelsPoint.offset.y !== 0
       },
       ableArrive (x, y) {
-        return x >= -1 && (x < 1 || (x === 1 && y === 0)) && y >= -1 && y <= 1
+        if (this.axisPoint.current.y % 2 === 0) {
+          return x >= -1 && (x < 1 || (x === 1 && y === 0)) && y >= -1 && y <= 1
+        }
+        return x <= 1 && (x > -1 || (x === -1 && y === 0)) && y >= -1 && y <= 1
       },
       startMove (ctx, x, y) {
         if (this.isClick && ctx.isPointInPath(this.pixelsPoint.click.x, this.pixelsPoint.click.y)) {
           if (this.ableArrive(x, y)) {
+            this.axisPoint.current.x += x
+            this.axisPoint.current.y += y
             let tx = this.axisPoint.old.x - x
             let ty = this.axisPoint.old.y - y
-            this.pixelsPoint.offset.x += (tx * this.width + (y % 2 === 0 ? 1 : -1) * (ty % 2 === 0 ? 0 : (this.width >> 1)))
+            this.pixelsPoint.offset.x += (tx * this.width + (y % 2 === 0 ? 1 : -1) * (ty % 2 === 0 ? 0 : (this.axisPoint.current.y % 2 === 0 ? -(this.width >> 1) : (this.width >> 1))))
             this.pixelsPoint.offset.y += ty * 1.5 * this.lengthen
             this.axisPoint.old.x = x
             this.axisPoint.old.y = y
@@ -66,7 +71,7 @@
       },
       axisToPixels (axis) {
         return {
-          x: (this.canvas.width >> 1) + axis.x * this.width + (axis.y % 2 === 0 ? 0 : this.halfWidth),
+          x: (this.canvas.width >> 1) + axis.x * this.width + (axis.y % 2 === 0 ? 0 : (this.axisPoint.current.y % 2 === 0 ? this.halfWidth : -this.halfWidth)),
           y: (this.canvas.height >> 1) + axis.y * ((this.lengthen << 1) - (this.lengthen >> 1))
         }
       },
@@ -89,27 +94,14 @@
         ctx.stroke()
       },
       drawText (ctx, px, py, text) {
-        if (!this.isMove()) {
-          if (ctx.isPointInPath(this.centerPixels.x, this.centerPixels.y)) {
-            ctx.fillStyle = 'wheat'
-            ctx.fill()
-            ctx.fillStyle = 'black'
-            ctx.fillText(text, px, py)
-//            this.drawText(ctx, 'wheat', lx, ly, x + ',' + y)
-//          } else {
-//            if (!forbid) {
-//            ctx.fillStyle = 'gray'
-//            } else {
-//            ctx.fillStyle = 'black'
-//            }
-//            ctx.fill()
-//            ctx.fillStyle = 'white'
-//            ctx.fillText(text, x, y)
-//            this.drawText(ctx, 'white', lx, ly, text)
-          } else {
-            ctx.fillStyle = 'wheat'
-            ctx.fillText(text, px, py)
-          }
+        if (ctx.isPointInPath(this.centerPixels.x - this.pixelsPoint.offset.x, this.centerPixels.y - this.pixelsPoint.offset.y)) {
+          ctx.fillStyle = '#388E8E'
+          ctx.fill()
+          ctx.fillStyle = 'black'
+          ctx.fillText(text, px, py)
+        } else {
+          ctx.fillStyle = '#388E8E'
+          ctx.fillText(text, px, py)
         }
       },
       offset (x, y) {
@@ -158,7 +150,7 @@
       this.canvas.height = this.canvas.parentNode.clientHeight
       let ctx = this.canvas.getContext('2d')
       this.pixelsPoint.click = {x: this.canvas.width >> 1, y: this.canvas.height >> 1}
-      ctx.strokeStyle = 'wheat'
+      ctx.strokeStyle = '#388E8E'
       ctx.textAlign = 'center'
       ctx.textBaseline = 'middle'
       ctx.font = '14px Arial'
@@ -170,13 +162,9 @@
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         for (let y = -startY; y < endY; y++) {
           for (let x = -startX; x < endX; x++) {
-//            if (y === -2 || y === 2 || x === -4 || x === 3) {
-//              this.drawMap(ctx, x, y, '', true)
-//            } else {
-            let name = x + ',' + y // this.maps.length === 0 ? '' : this.maps[y + 1].map[x + 3].name
+            let name = (x + this.axisPoint.current.x) + ',' + (y + this.axisPoint.current.y) // this.maps.length === 0 ? '' : this.maps[y + 1].map[x + 3].name
             let forbid = false // this.maps.length === 0 ? true : this.maps[y + 1].map[x + 3].forbid
             this.drawMap(ctx, x, y, name, forbid)
-//            }
           }
         }
         window.requestAnimationFrame(fn)
@@ -189,7 +177,8 @@
         isClick: false,
         maps: {size: {x: 7, y: 5}, cells: []},
         axisPoint: {
-          old: {x: 0, y: 0}
+          old: {x: 0, y: 0},
+          current: {x: 0, y: 0}
         },
         pixelsPoint: {
           offset: {x: 0, y: 0},
