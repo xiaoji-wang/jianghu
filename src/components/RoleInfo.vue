6554dd4c220ab95ef9f4385b9a015da71e7e7126
wxji <vmoa5304>
2017-07-31 18:18:14 +0800

    1

--- src/components/RoleInfo.vue
+++ src/components/RoleInfo.vue
@@ -1,8 +1,15 @@
 <template>
-  <div style="width: 100%;height: 100%;top: 0;background: black;">
+  <div class="container">
     <div class="close">
       <router-link to="/world">关闭</router-link>
     </div>
+    王大明
+    生辰
+    生命
+    内力
+    力量
+    身法
+    悟性
   </div>
 </template>
 
@@ -18,9 +25,21 @@
 </script>
 
 <style scoped>
+  .container {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: absolute;
+    background: black;
+    border: solid 0.1rem #388E8E;
+    color: #388E8E;
+  }
+
   .close {
     position: absolute;
-    border: solid 0.1rem #388E8E;
+    border-left: solid 0.1rem #388E8E;
+    border-bottom: solid 0.1rem #388E8E;
     width: 2.5rem;
     line-height: 2.5rem;
     right: 0;
