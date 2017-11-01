a716d2819122a03562a7a46ca2e6944bb0ca98ed
wxji <vmoa5304>
2017-11-01 18:13:55 +0800

    1

--- src/components/Fight.vue
+++ src/components/Fight.vue
@@ -6,6 +6,7 @@
           <div class="hp" :style="{width:refreshHp(l.id,'LEFT')}"></div>
           <div class="cd" :style="{width:refreshCD(l.id,'LEFT')}"></div>
           <div :class="{dead:l.hp<=0}">{{l.name}}</div>
+          <div class="hurt">-{{hurt}}</div>
         </div>
       </div>
       <div class="right">
@@ -13,15 +14,16 @@
           <div class="hp" :style="{width:refreshHp(r.id,'RIGHT')}"></div>
           <div class="cd" :style="{width:refreshCD(r.id,'RIGHT')}"></div>
           <div :class="{dead:r.hp<=0}">{{r.name}}</div>
+          <div class="hurt">-{{hurt}}</div>
         </div>
       </div>
     </div>
     <div class="buttonDiv" style="text-align: center;">
-      <router-link to="/world/map" replace v-show="!refresh">逃跑</router-link>
-      <router-link to="/world/map" replace v-show="refresh">返回</router-link>
+      <router-link to="/world/map" replace v-show="refresh">逃跑</router-link>
+      <router-link to="/world/map" replace v-show="!refresh">返回</router-link>
     </div>
     <div class="text">
-      <!--<div v-for="s in consoles" style="margin: 0.1rem">{{s}}</div>-->
+      <div v-for="s in consoles" style="margin: 0.1rem" v-html="s"></div>
     </div>
   </div>
 </template>
@@ -33,11 +35,15 @@
     data () {
       return {
         refresh: true,
-        left: [{id: 1, name: '王大明', hp: 100, cd: 0, attackSpeed: 5, location: 'LEFT', attack: 50}],
+        hurt: 0,
+//        s: '<div v-for="s in consoles" style="margin: 0.1rem"></div>',
+        consoles: [],
+        left: [
+          {id: 1, name: '主角', maxHp: 10, hp: 10, cd: 0, attackSpeed: 3, location: 'LEFT', attack: 4},
+          {id: 5, name: '跟班', maxHp: 10, hp: 10, cd: 0, attackSpeed: 2, location: 'LEFT', attack: 4}
+        ],
         right: [
-          {id: 2, name: '村民1', hp: 100, cd: 0, attackSpeed: 1, location: 'RIGHT', attack: 5},
-          {id: 3, name: '村民2', hp: 100, cd: 0, attackSpeed: 3, location: 'RIGHT', attack: 5},
-          {id: 4, name: '村民3', hp: 100, cd: 0, attackSpeed: -1, location: 'RIGHT', attack: 5}
+          {id: 2, name: '村民1', maxHp: 10, hp: 10, cd: 0, attackSpeed: 1, location: 'RIGHT', attack: 2}
         ]
       }
     },
@@ -68,76 +74,100 @@
             return r
           }
         })
-        return r[0].hp + '%'
+        if (r[0].hp <= 0) {
+          return 0
+        }
+        return Math.floor(r[0].hp / r[0].maxHp * 100) + '%'
       }
     },
     mounted () {
-      let bool = true
+      this.consoles.push('主角一抱拳，朗声说道：“请赐教！”')
       let fn = () => {
+        let bool = true
+        let arr = this.left.concat(this.right)
+        let r
+        for (let i = 0; i < arr.length; i++) {
+          r = arr[i]
+          if (r.hp > 0) {
+            r.cd += (1 + r.attackSpeed / 10)
+            if (r.cd >= 100) {
+              bool = false
+              break
+            }
+          }
+        }
         if (bool) {
-          this.left.concat(this.right).filter((v) => {
+          window.requestAnimationFrame(fn)
+          return
+        }
+        r.cd = 0
+        let alive = []
+        if (r.location === 'LEFT') {
+          alive = this.right.filter((v) => {
             if (v.hp > 0) {
               return v
             }
-          }).forEach((r) => {
-            r.cd += (2 + r.attackSpeed / 10)
-            if (r.cd >= 100) {
-              bool = false
-              r.cd = 0
-              let alive = []
-              if (r.location === 'LEFT') {
-                alive = this.right.filter((v) => {
-                  if (v.hp > 0) {
-                    return v
-                  }
-                })
-              } else {
-                alive = this.left.filter((v) => {
-                  if (v.hp > 0) {
-                    return v
-                  }
-                })
-              }
-              if (alive.length > 0) {
-                let target = alive[Math.floor(Math.random() * alive.length)]
-                let e = document.getElementById('r' + r.id)
-                let t = document.getElementById('r' + target.id)
-                Velocity(e, {
-                  left: (t.offsetLeft - e.offsetLeft) + 'px',
-                  top: (t.offsetTop - e.offsetTop) + 'px'
-                }, {
-                  duration: 100,
-                  complete: () => {
-                    target.hp = target.hp - r.attack
-                    Velocity(e, {left: 0, top: 0}, {
-                      duration: 100,
-                      complete: () => {
-                        Velocity(t, {
-                          left: '0.1rem'
-                        }, {
-                          loop: 3,
-                          duration: 10,
-                          complete: () => {
-                            if (target.hp <= 0) {
-                              target.cd = 0
-                              if (alive.length === 1) {
-                                this.refresh = false
-                                window.console.info('stop')
-                              }
-                            }
-                            bool = true
-                          }
-                        })
-                      }
-                    })
-                  }
-                })
-              }
+          })
+        } else {
+          alive = this.left.filter((v) => {
+            if (v.hp > 0) {
+              return v
             }
           })
         }
-        if (this.refresh) {
-          window.requestAnimationFrame(fn)
+        if (alive.length > 0) {
+          let target = alive[Math.floor(Math.random() * alive.length)]
+          let e = document.getElementById('r' + r.id)
+          let t = document.getElementById('r' + target.id)
+          let h = t.getElementsByClassName('hurt')[0]
+          // 攻击
+          Velocity(e, {
+            left: (t.offsetLeft - e.offsetLeft) + 'px', top: (t.offsetTop - e.offsetTop) + 'px'
+          }, {
+            duration: 100,
+            complete: () => {
+              this.hurt = r.attack
+              target.hp = target.hp - r.attack
+              this.consoles.push(r.name + '对' + target.name + '使出了一招<span style="color: #388e8e;">【普通的一拳】</span>，造成了' + r.attack + '点伤害')
+            }
+          })
+          // 返回
+          Velocity(e, {left: 0, top: 0}, {duration: 100})
+          // 振动
+          Velocity(t, {
+            left: '0.1rem'
+          }, {
+            loop: 3,
+            duration: 20,
+            complete: () => {
+              h.style.display = 'block'
+            }
+          })
+          // 飘血
+          Velocity(h, {
+            top: '-2rem', opacity: 0
+          }, {
+            duration: 900,
+            complete: () => {
+              let isOver = false
+              if (target.hp <= 0) {
+                target.cd = 0
+                if (alive.length <= 1) {
+                  isOver = true
+                }
+              }
+              if (!isOver) {
+                h.style.display = 'none'
+                h.style.top = 0
+                h.style.opacity = 1
+                this.hurt = 0
+                window.requestAnimationFrame(fn)
+              }
+            }
+          })
+        } else {
+          // TODO over
+          this.refresh = false
         }
       }
       fn()
@@ -193,5 +223,29 @@
   .dead {
     text-decoration: line-through;
     color: #666;
+  }
+
+  .hurt {
+    position: absolute;
+    color: red;
+    text-align: center;
+    width: 100%;
+    font-size: 0.8em;
+    font-weight: bold;
+    top: 0rem;
+    display: none;
+  }
+
+  .text {
+    padding: 0.2rem;
+    overflow-y: auto;
+    font-size: 0.8em;
+    color: #ccc;
+    position: absolute;
+    top: 24.5rem;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #222;
   }
 </style>
